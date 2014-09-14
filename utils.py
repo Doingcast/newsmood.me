ed11449edd819e50ba0b35e4f095552b73c620aa
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-09-14 00:35:33 -0700

    parser_main_content

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import requests
 from datetime import date, timedelta
 
 HP_KEY = '08002bd7-6107-41a6-8aef-d067dc823d70'
+PARSER_TOKEN = '841f04fafb770c310d4aa9876a10a155ea922817'
 initial_date = date.today() - timedelta(days = 30)
 
 def get_relative_news(text):
@@ -18,3 +19,14 @@ def get_relative_news(text):
 
     return resp.json()
 
+def parser_main_content(url):
+    params = {
+        'url' : url,
+        'token' : PARSER_TOKEN,
+    }
+    resp = requests.get(
+        "https://readability.com/api/content/v1/parser",
+        params = params
+    )
+
+    return resp.json()
