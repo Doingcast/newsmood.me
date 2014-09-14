890d15b2e6afe5c913ff8f3c6439a89bae0ef433
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-09-14 02:26:08 -0700

    requirements e view in webapp2

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import webapp2
+from utils import get_news_mood
+import json
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, webapp2!')
 
+class GetJson(webapp2.RequestHandler):
+    def get(self):
+        text = self.request.get('text')
+        if text:
+            resp = get_news_mood(text)
+        else:
+            resp = ""
+
+        self.response.write(json.dumps(resp))
+
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
+    ('/get', GetJson),
     ], debug=True)
