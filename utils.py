2741eaf42c2b0f719b4c73b2b41e32fcb240eca4
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-09-14 14:29:06 -0700

    changing from requests to urlparse, so it can be used on google app engine

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-import requests
+import webapp2
+from webapp2_extras import jinja2
+import urllib
+from google.appengine.api import urlfetch
 from datetime import date, timedelta
 from HTMLParser import HTMLParser
+import json
+import logging
 
 HP_KEY = '08002bd7-6107-41a6-8aef-d067dc823d70'
 PARSER_TOKEN = '841f04fafb770c310d4aa9876a10a155ea922817'
@@ -12,20 +17,21 @@ def get_relative_news(text):
     """
     params = {
         'indexes': 'news_eng',
-        'text': text,
+        'text': text.encode('utf8'),
         'min_date': initial_date.strftime('%Y-%m-%d'),
         'apikey': HP_KEY,
+        'summary': 'context'
     }
-    resp = requests.get(
-        "https://api.idolondemand.com/1/api/sync/querytextindex/v1",
-        params = params
+    form_data = urllib.urlencode(params)
+    resp = urlfetch.fetch(
+        url = "https://api.idolondemand.com/1/api/sync/querytextindex/v1?%s" % form_data,
+        method= urlfetch.GET,
+        deadline=60
     )
-    try:
-        return resp.json()['documents']
-    except:
-        print resp
-        print text
-        return "{}"
+    resp = json.loads(resp.content)
+    logging.info("### get_relative_news ###")
+    logging.info(resp)
+    return resp['documents']
 
 def parser_main_content(url):
     """
@@ -34,17 +40,17 @@ def parser_main_content(url):
     params = {
         'url' : url,
         'token' : PARSER_TOKEN,
-    }
-    resp = requests.get(
-        "https://readability.com/api/content/v1/parser",
-        params = params
+        }
+    form_data = urllib.urlencode(params)
+    resp = urlfetch.fetch(
+        url = "https://readability.com/api/content/v1/parser?%s" % form_data,
+        method = urlfetch.GET,
+        deadline=60
     )
-    try:
-        return resp.json()
-    except:
-        print resp
-        print url
-        return "{}"
+    resp = json.loads(resp.content)
+    logging.info("### parser_main_content ###")
+    # logging.info(resp.content)
+    return resp
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -86,20 +92,21 @@ def get_mood(text, lang = 'eng'):
     Returns the mood of a given text
     """
     params = {
-        'text': text,
+        'text': text.encode('utf8'),
         'apikey': HP_KEY,
         'lang': lang
     }
-    resp = requests.get(
-        "https://api.idolondemand.com/1/api/sync/analyzesentiment/v1",
-        params = params
+    form_data = urllib.urlencode(params)
+    resp = urlfetch.fetch(
+        url = "https://api.idolondemand.com/1/api/sync/analyzesentiment/v1",
+        method = urlfetch.POST,
+        payload=form_data,
+        deadline=60
     )
-    try:
-        return resp.json()
-    except:
-        print resp
-        print text
-        return "{}"
+    resp = json.loads(resp.content)
+    logging.info("### get_mood ###")
+    logging.info(resp)
+    return resp
 
 def get_news_mood(text):
     """
@@ -112,8 +119,8 @@ def get_news_mood(text):
     for news in get_relative_news(text):
         main_content = "%s %s" % (
             news['title'],
-            strip_tags(parser_main_content(news['reference'])['content'])
-        )
+            strip_tags(parser_main_content(news['reference'])['content'].replace('\n',' ').replace('  ',' '))
+            )
         m = get_mood(main_content)
         moods.append(
             {
@@ -121,6 +128,19 @@ def get_news_mood(text):
                 'sentiment': m['aggregate']['sentiment'],
                 'url': news['reference'],
                 'title': news['title'],
+                'summary':news['summary']
             }
         )
     return moods
+
+
+class BaseHandler(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        # Returns a Jinja2 renderer cached in the app registry.
+        return jinja2.get_jinja2(app=self.app)
+
+    def render_response(self, _template, **context):
+        # Renders a template and writes the result to the response.
+        rv = self.jinja2.render_template(_template, **context)
+        self.response.write(rv)
