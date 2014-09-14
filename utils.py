ef44ca8ae0e6fe3d5cc2f218d137115f8a1e0dbd
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-09-14 01:58:09 -0700

    get_news_mood

--- utils.py
+++ utils.py
@@ -7,6 +7,9 @@ PARSER_TOKEN = '841f04fafb770c310d4aa9876a10a155ea922817'
 initial_date = date.today() - timedelta(days = 30)
 
 def get_relative_news(text):
+    """
+    Returns a list of news related to the text
+    """
     params = {
         'indexes': 'news_eng',
         'text': text,
@@ -17,10 +20,17 @@ def get_relative_news(text):
         "https://api.idolondemand.com/1/api/sync/querytextindex/v1",
         params = params
     )
-
-    return resp.json()
+    try:
+        return resp.json()['documents']
+    except:
+        print resp
+        print text
+        return "{}"
 
 def parser_main_content(url):
+    """
+    Extract the main content of a given URL
+    """
     params = {
         'url' : url,
         'token' : PARSER_TOKEN,
@@ -29,8 +39,12 @@ def parser_main_content(url):
         "https://readability.com/api/content/v1/parser",
         params = params
     )
-
-    return resp.json()
+    try:
+        return resp.json()
+    except:
+        print resp
+        print url
+        return "{}"
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -68,6 +82,9 @@ def strip_tags(value):
     return value
 
 def get_mood(text, lang = 'eng'):
+    """
+    Returns the mood of a given text
+    """
     params = {
         'text': text,
         'apikey': HP_KEY,
@@ -77,4 +94,33 @@ def get_mood(text, lang = 'eng'):
         "https://api.idolondemand.com/1/api/sync/analyzesentiment/v1",
         params = params
     )
-    return resp.json()
+    try:
+        return resp.json()
+    except:
+        print resp
+        print text
+        return "{}"
+
+def get_news_mood(text):
+    """
+    It does the magic.
+        1. Get all news relative to the text
+        2. Strip the best content of them
+        3. Get_mood for those contents
+    """
+    moods = []
+    for news in get_relative_news(text):
+        main_content = "%s %s" % (
+            news['title'],
+            strip_tags(parser_main_content(news['reference'])['content'])
+        )
+        m = get_mood(main_content)
+        moods.append(
+            {
+                'score': m['aggregate']['score'],
+                'sentiment': m['aggregate']['sentiment'],
+                'url': news['reference'],
+                'title': news['title'],
+            }
+        )
+    return moods
